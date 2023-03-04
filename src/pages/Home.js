d6faf1cfe9a0cf1b6f00923bcd66237c608c1c7b
Hatice-Altun <van.altun@outlook.com>
2023-03-04 15:54:17 +0100

    Styles Border-Radius

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,23 @@ import React from "react";
 import {Link} from "react-router-dom";
 import BannerImage from "../assets/macaron.jpg";
 import "../styles/Home.css";
+import Macarons from "./Macarons";
+import Contact from "./Contact";
 
 function Home() {
     return (
-        <div className="home" style={{backgroundImage: `url(${BannerImage})`}}>
-            <div className="headerContainer">
-                <h1> Altun Macarons </h1>
-
-                <Link to="/macarons">
-                    <button> Let's Taste!</button>
-                </Link>
+        <>
+            <div className="home" style={{backgroundImage: `url(${BannerImage})`}}>
+                <div className="headerContainer">
+                    <h1> Altun Macarons </h1>
+                    <Link to="/macarons">
+                        <button> Let's Taste!</button>
+                    </Link>
+                </div>
             </div>
-        </div>
+            <Macarons/>
+            <Contact/>
+        </>
     );
 }
 
